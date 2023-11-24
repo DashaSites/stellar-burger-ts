1e5ec63e4c26b1c0f9dd5d5f8f40db25f8569d30
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2023-11-24 20:50:12 +0200

    added router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { HomePage } from '../../pages/home/home';
 
 
 function App() {
+
+  const location = useLocation();
+
+  const background = location.state && location.state.background;
+
+
+
   return (
   <div>
     <p>SOME TEXT</p>
+    <Routes location={background || location}>
+              <Route path="/" element={<HomePage />} />
+
+            </Routes>
 
     </div>
   );
